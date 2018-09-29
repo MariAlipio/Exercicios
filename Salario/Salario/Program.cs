44ad7d45a144b27141fc474b64cf2df8c83dc245
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-09-29 10:48:18 -0300

    Add files via upload

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario meuSalario = new Salario();
+            meuSalario.Salario_base = 1000; 
+
+            Console.WriteLine("O salario liquido é:" + meuSalario.SalarioLiquido(meuSalario.Salario_base,0.05,0.07));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Salario/Salario/Salario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salario
+{
+    /*Faça um algoritmo que receba o salário-base de um funcionário, 
+     * calcule e mostre o salário a receber, 
+     * sabendo-se que esse funcionário tem gratificação de 5% sobre o salário-base e paga imposto de 7% também sobre o salário-base.
+     */
+    class Salario
+    {
+        private double salario_base;
+        static readonly double gratificação = 0.05;
+        static readonly double imposto = 0.07;
+
+        public Salario(double salario_base)
+        {
+            Salario_base = this.salario_base;
+        }
+
+        public Salario()
+        {
+        }
+
+        public static double Gratificação => gratificação;
+
+        public static double Imposto => imposto;
+
+        public double Salario_base { get => salario_base; set => salario_base = value; }
+
+        public double SalarioLiquido(double salario_base, double gratificação, double imposto) => (Salario_base + (Salario_base * Gratificação) - (salario_base * Imposto));
+
+    }
+}
