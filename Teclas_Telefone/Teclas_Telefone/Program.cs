858b76bf8a6e7c3a2bdfea4e83ca64ded87b6cde
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-10-12 11:28:52 -0300

    Add files via upload

---
+++ Teclas_Telefone/Teclas_Telefone/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Teclas_Telefone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[] { 33, 8, 88, 777, 66, 0, 9, 33, 22, 0, 33, 0, 7777, 666, 333, 8, 9, 2, 777, 33, 0, 7777, 666, 555, 88, 8, 444, 666, 66 };
+
+            Convert.ToString(vet[29]);
+
+            //criar um metodo que retorna o valor naquela posição e chamar esse metodo no for
+            //trocar esse valor pela letra
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                int var = vet[i];
+
+                
+            }
+           
+        }
+    }
+}
